32add612e59b7e77642ac8dd2519320bd6dda8fc
Anshuman Singh <ps968976@gmail.com>
2022-05-04 13:47:30 +0530

    Initial Commit

---
+++ game.js
@@ -0,0 +1,135 @@
+// console.log("Hello Gamer")
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+// const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
+
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+// question.json file is the inner file
+// fetch("questions.json")
+// fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple")
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+  .then((res) => {
+    // console.log(res);
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map(loadedQuestion => {
+        const formattedQuestion = {
+            question: loadedQuestion.question
+        };
+
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        // Import number from 0-9
+        formattedQuestion.answer = Math.floor(Math.random()*10) + 1;
+        answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index-1)] = choice;
+        });
+        return formattedQuestion;
+    })
+    
+    startGame();
+  })
+  .catch(err => {
+      console.error(err);
+  });
+// CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 10;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+
+   // Loading Question
+  getNewQuestion();
+  
+  // Loader & Question comming
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
+
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+
+    // Go to the End Page.
+    return window.location.assign("/end.html");
+
+  }
+  questionCounter++;
+
+  // Updating Question eg:- 1/3
+  // questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+  // Updating ProgressBar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    // console.log(e.target);
+    if (!acceptingAnswers) return;
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    // const classToApply = 'incorrect';
+    // if (selectedAnswer == currentQuestion.answer) {
+    //     classToApply = 'correct';
+    // }
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    // Calling Score
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+    // console.log(classToApply);
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+// Increment in score:-
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
